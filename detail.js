aad27d92cdbd777fa7f0ef717ee05ad90e31e9a4
socram63 <marcos_bep@hotmail.com>
2024-03-29 09:16:01 +0000

    adding map and route to details screen

--- detail.js
+++ detail.js
@@ -2,40 +2,56 @@ const params = new URLSearchParams(window.location.search)
 const rideID = params.get("id")
 const ride = getRideRecord(rideID)
 
-document.addEventListener("DOMContentLoaded", async ()=>{
+document.addEventListener("DOMContentLoaded", async () => {
 
-const fristPosition = ride.data[0]
-const fristLocationData = await getLocationData(fristPosition.latitude, fristPosition.longitude)
-
-
-const dataElement = document.createElement("div")
-dataElement.className = "flex-fill d-flex flex-column"
+    const fristPosition = ride.data[0]
+    const fristLocationData = await getLocationData(fristPosition.latitude, fristPosition.longitude)
 
 
-const cityDiv = document.createElement("div")
-cityDiv.innerText = `${fristLocationData.city} - ${fristLocationData.countryCode}`
-cityDiv.className = "text-primary mb-2"
+    const dataElement = document.createElement("div")
+    dataElement.className = "flex-fill d-flex flex-column"
 
-const maxSpeedDiv = document.createElement("div")
-maxSpeedDiv.innerText = `Max speed: ${getMaxSpeed(ride.data)} Km/h`
-maxSpeedDiv.className = "h5"
 
-const distanceDiv = document.createElement("div")
-distanceDiv.innerText = `Distance: ${getDistance(ride.data)} km`
+    const cityDiv = document.createElement("div")
+    cityDiv.innerText = `${fristLocationData.city} - ${fristLocationData.countryCode}`
+    cityDiv.className = "text-primary mb-2"
 
-const durationDiv = document.createElement("div")
-durationDiv.innerText = `Duration: ${getDuration(ride)}`
+    const maxSpeedDiv = document.createElement("div")
+    maxSpeedDiv.innerText = `Max speed: ${getMaxSpeed(ride.data)} Km/h`
+    maxSpeedDiv.className = "h5"
 
-const dateDiv = document.createElement("div")
-dateDiv.innerText = getStartDate(ride)
-dateDiv.className = "text-secondary mt-2"
+    const distanceDiv = document.createElement("div")
+    distanceDiv.innerText = `Distance: ${getDistance(ride.data)} km`
 
-dataElement.appendChild(cityDiv)
-dataElement.appendChild(maxSpeedDiv)
-dataElement.appendChild(distanceDiv)
-dataElement.appendChild(durationDiv)
-dataElement.appendChild(dateDiv)
+    const durationDiv = document.createElement("div")
+    durationDiv.innerText = `Duration: ${getDuration(ride)}`
 
-document.querySelector("#data").appendChild(dataElement)
+    const dateDiv = document.createElement("div")
+    dateDiv.innerText = getStartDate(ride)
+    dateDiv.className = "text-secondary mt-2"
 
+    dataElement.appendChild(cityDiv)
+    dataElement.appendChild(maxSpeedDiv)
+    dataElement.appendChild(distanceDiv)
+    dataElement.appendChild(durationDiv)
+    dataElement.appendChild(dateDiv)
+
+    document.querySelector("#data").appendChild(dataElement)
+
+    const map = L.map("mapDetail")
+    map.setView([fristPosition.latitude, fristPosition.longitude], 13)
+    L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_toner_background/{z}/{x}/{y}{r}.{ext}', {
+        minZoom: 5,
+        maxZoom: 20,
+        attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        ext: 'png'
+    }).addTo(map);
+
+    const positionsArray = ride.data.map((position => {
+        return [position.latitude, position.logitude]
+    }))
+    const polyline = L.polyline(positionsArray, { color: "#F00" })
+    polyline.addTo(map)
+
+    map.fitBounds(polyline.getBounds())
 })
